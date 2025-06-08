5b63e09578f776f367fd63cac8b64c98958aa699
Andrey Litvitski <andrey1010102008@gmail.com>
2025-06-08 18:10:17 +0300

    new solution

---
+++ leetcodegpu/ReverseArray/ReverseArray.cu
@@ -0,0 +1,20 @@
+#include "solve.h"
+#include <cuda_runtime.h>
+
+__global__ void reverse_array(float* input, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N / 2) {
+        float buff = input[idx];
+        input[idx] = input[N - idx - 1];
+        input[N - idx - 1] = buff;
+    }
+}
+
+// input is device pointer
+void solve(float* input, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N / 2 + threadsPerBlock - 1) / threadsPerBlock;
+
+    reverse_array<<<blocksPerGrid, threadsPerBlock>>>(input, N);
+    cudaDeviceSynchronize();
+}
