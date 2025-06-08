601f36612b150b355ede24606685387ba3fa3c87
Andrey Litvitski <andrey1010102008@gmail.com>
2025-06-08 18:16:08 +0300

    new solution

---
+++ VectorAddition/VectorAddtion.cu
@@ -0,0 +1,18 @@
+#include "solve.h"
+#include <cuda_runtime.h>
+
+__global__ void vector_add(const float* A, const float* B, float* C, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < N) {
+        C[idx] = A[idx] + B[idx];
+    }
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+void solve(const float* A, const float* B, float* C, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    vector_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    cudaDeviceSynchronize();
+}
